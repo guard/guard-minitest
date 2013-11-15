b20bb7de6441a0d09c9c5cbe2d5120faca2cb3ad
Rémy Coutable <remy@rymai.me>
2013-11-15 19:20:05 +0100

    Bump to 2.0.1

--- lib/guard/minitest/version.rb
+++ lib/guard/minitest/version.rb
@@ -1,7 +1,7 @@
 module Guard
   class MinitestVersion
 
-    VERSION = '2.0.0'
+    VERSION = '2.0.1'
 
   end
 end
--- spec/guard/minitest/version_spec.rb
+++ spec/guard/minitest/version_spec.rb
@@ -5,7 +5,7 @@ describe Guard::MinitestVersion do
 
   describe 'VERSION' do
     it 'is the current version' do
-      Guard::MinitestVersion::VERSION.must_equal '2.0.0'
+      Guard::MinitestVersion::VERSION.must_equal '2.0.1'
     end
   end
 
