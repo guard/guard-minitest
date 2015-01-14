b373f84da1c17a8071dbabe27e26ca0f35b4930e
Cezary Baginski <cezary@chronomantic.net>
2015-01-14 17:43:37 +0100

    remove version test (only causes merge issues)

--- spec/lib/guard/minitest/version_spec.rb
+++
@@ -1,7 +0,0 @@
-RSpec.describe Guard::MinitestVersion do
-  describe 'VERSION' do
-    it 'is the current version' do
-      expect(Guard::MinitestVersion::VERSION).to eq '2.3.2'
-    end
-  end
-end
