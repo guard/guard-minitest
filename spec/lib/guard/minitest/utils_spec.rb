1a3c359a78a95aeb25a84c484b071a6f51286679
Cezary Baginski <cezary@chronomantic.net>
2015-02-03 22:07:42 +0100

    fix support for minitest 4

--- spec/lib/guard/minitest/utils_spec.rb
+++ spec/lib/guard/minitest/utils_spec.rb
@@ -19,5 +19,18 @@ RSpec.describe Guard::Minitest::Utils do
 
       system(*%w(bundle exec ruby -e) + [code])
     end
+
+    if !Guard::Minitest::Utils.minitest_version_gte_5?
+      context 'when runner for old minitest is used' do
+        it "load guard automatically" do
+          code = <<-EOS
+        require 'guard/minitest/runners/old_runner'
+        exit 0
+          EOS
+
+          expect(system(*%w(bundle exec ruby -e) + [code])).to eq(true)
+        end
+      end
+    end
   end
 end
